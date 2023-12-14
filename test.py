a59a8edf666d3a102e1a82bd07f41d3d90f65ed9
Beorlor <jbdurand2000@gmail.com>
2023-12-14 14:22:42 +0100

    test

---
+++ test.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+# Initialize Chrome WebDriver
+driver = webdriver.Chrome()
+
+# Open a webpage
+driver.get("https://www.google.com")
+
+# Close the browser window
+# driver.quit()
